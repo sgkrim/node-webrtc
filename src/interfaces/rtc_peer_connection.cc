6634fce209a1f2f0d63c86df3a76521c9d60d9a7
Dev Master <smedia.developer@gmail.com>
2025-07-27 17:41:31 +0300

    change links

--- src/interfaces/rtc_peer_connection.cc
+++ src/interfaces/rtc_peer_connection.cc
@@ -57,18 +57,16 @@
 namespace node_webrtc {
 
 cricket::MediaChannel* RTCPeerConnection::GetMediaChannel(const std::string& trackId) {
-  if (!_pc) {
+  if (!_jinglePeerConnection) { // <--- ВИПРАВЛЕНО
     return nullptr;
   }
-  auto* channel_manager = _pc->channel_manager();
+  auto* channel_manager = _jinglePeerConnection->channel_manager(); // <--- ВИПРАВЛЕНО
   if (!channel_manager) {
     return nullptr;
   }
-  // Спочатку шукаємо в аудіо-каналах
   if (auto* channel = channel_manager->GetVoiceChannel(trackId)) {
     return channel;
   }
-  // Якщо не знайшли, шукаємо у відео-каналах
   if (auto* channel = channel_manager->GetVideoChannel(trackId)) {
     return channel;
   }
